919a9602d347e2047dadcbe61b62a9690a5ed6f1
oedwin <onyangoedwin898@gmail.com>
2017-04-05 10:47:16 +0300

    Added login, signup, logout, some css

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 
 # creates a model form to save the results of the form to the model
@@ -7,3 +7,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'headline', 'text')
+
+
+#enables users to add comments
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
